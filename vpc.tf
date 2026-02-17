2b07cb14e6326807c414c8b0a3956d7c0179ba85
Harshada732 <harshadaghadwaje629@gmail.com>
2026-02-17 13:48:47 +0530

    added vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,15 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "three-tier-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "three-tier-igw"
+  }
+}
